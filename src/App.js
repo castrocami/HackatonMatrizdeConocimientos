50e23bed8e06d9f2ee8312f19341e0866f124c54
Camila Castro <ccastromn@gmail.com>
2021-07-14 12:32:14 -0400

    Chore: Firebase configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup?authuser=0
+const firebaseConfig = {
+  apiKey: "AIzaSyC3karrBdqmSf_LCSUrOSjkDe8pW6dhNoQ",
+  authDomain: "matriz-de-conocimientos.firebaseapp.com",
+  projectId: "matriz-de-conocimientos",
+  storageBucket: "matriz-de-conocimientos.appspot.com",
+  messagingSenderId: "617798514262",
+  appId: "1:617798514262:web:3daf80159d359aaf562df7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
