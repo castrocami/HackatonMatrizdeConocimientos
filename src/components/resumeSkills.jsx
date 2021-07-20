b6d79ba8f34ea018a0ae9dd51527bd2f9c67f09a
Camila Castro <ccastromn@gmail.com>
2021-07-20 18:36:06 -0400

    Feat: Display resume-skills

--- src/components/resumeSkills.jsx
+++ src/components/resumeSkills.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import "firebase/firestore";
 import { useFirebaseApp } from 'reactfire';
+import { useState, useEffect } from 'react';
 
 const ResumeSkills = props => {
-
+    const [newArray, setNewArray] = useState([]); 
     const firebase = useFirebaseApp();
-    const user = firebase.auth().currentUser.email
+    const user = firebase.auth().currentUser.email;
     const db = firebase.firestore();
-  
-    db.collection('users').where('user.email', '==', user).get().then((querySnapshot) => {
-      console.log(querySnapshot.docs);
-      querySnapshot.forEach((doc) => {
-        console.log("Dataaa" , doc.data());
-      });
-    });
-    
+
+    useEffect(()=>{
+        db.collection('users').where('user.email', '==', user).get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              const userData = doc.data().user;
+              const keysArray = Object.keys(userData);
+              const tempArray = [] 
+              keysArray.forEach((element) => {
+                if(userData[element] !== "Nulo" && element !== "employer_name" && element !== "name" && element !== "email" && element !== "id" && element !== "undefined" ) {
+                    tempArray.push([element]);
+                }
+              })
+              setNewArray(tempArray);
+            });
+          });
+    },[user]);
+
     return (
         <div>
-            <h1>Skills</h1>
-        </div>
-    );
+          {newArray.map((element) => <h1>{element}</h1>)}
+      </div>
+  );
 };
-
 
 export default ResumeSkills;
