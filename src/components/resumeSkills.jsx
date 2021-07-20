67a569c6158e61dad084c666741c2843ffe47f29
Camila Castro <ccastromn@gmail.com>
2021-07-20 16:42:35 -0400

    Feat: Get data from firebase

--- src/components/resumeSkills.jsx
+++ src/components/resumeSkills.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import "firebase/firestore";
+import { useFirebaseApp } from 'reactfire';
 
 const ResumeSkills = props => {
+
+    const firebase = useFirebaseApp();
+    const user = firebase.auth().currentUser.email
+    const db = firebase.firestore();
+  
+    db.collection('users').where('user.email', '==', user).get().then((querySnapshot) => {
+      console.log(querySnapshot.docs);
+      querySnapshot.forEach((doc) => {
+        console.log("Dataaa" , doc.data());
+      });
+    });
+    
     return (
         <div>
             <h1>Skills</h1>
